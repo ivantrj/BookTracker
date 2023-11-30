352e9de2373d9fc4d910316f78d537d6990c6992
Ivan Trajanovski ruwido <ivan.trajanovski@ruwido.com>
2023-11-30 15:31:57 +0100

    Initial Commit

---
+++ BookTracker/BookTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTrackerApp.swift
+//  BookTracker
+//
+//  Created by ivan ruwido  on 30.11.23.
+//
+
+import SwiftUI
+
+@main
+struct BookTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
