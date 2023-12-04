f10e40aad8969950f92960f54a49f286b3ebb46a
ivan <ivantrajanovski@gmail.com>
2023-12-04 14:31:03 +0100

    swipe to delete

--- BookTracker/Views/InboxView.swift
+++ BookTracker/Views/InboxView.swift
@@ -11,46 +11,54 @@ struct InboxView: View {
     @AppStorage("darkMode") var isDarkMode = false
     @Environment(\.managedObjectContext) var managedObjContext
     @FetchRequest(sortDescriptors: [SortDescriptor(\.date, order: .reverse)]) var books: FetchedResults<Book>
-
+    
     @State private var showingAddBookView = false // State to control the presentation of the add book view
-
+    
     var body: some View {
-        ZStack {
-            NavigationStack {
-                VStack {
-                    ScrollView(.vertical, showsIndicators: true) {
-                        VStack {
-                            ForEach(books) { book in
-                                BookDetail(title: book.title ?? "", author: book.author ?? "")
-                            }
-                        }
-                    }
+        NavigationStack {
+            List {
+                ForEach(books) { book in
+                    BookDetail(title: book.title ?? "", author: book.author ?? "")
+                }
+                .onDelete(perform: deleteBook)
+            }
+            .padding()
+            .navigationTitle("Inbox")
+            .preferredColorScheme(isDarkMode ? .dark : .light)
+        }
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                Button(action: {
+                    showingAddBookView = true
+                }) {
+                    Image(systemName: "plus")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
                 }
                 .padding()
-                .navigationTitle("Inbox")
-                .preferredColorScheme(isDarkMode ? .dark : .light)
-            }
-            VStack {
-                Spacer()
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        showingAddBookView = true
-                    }) {
-                        Image(systemName: "plus")
-                            .resizable()
-                            .frame(width: 24, height: 24)
-                            .padding()
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .clipShape(Circle())
-                    }
-                    .padding()
-                    .sheet(isPresented: $showingAddBookView) {
-                        AddBookView()
-                    }
+                .sheet(isPresented: $showingAddBookView) {
+                    AddBookView()
                 }
             }
+        }
+    }
+    
+    func deleteBook(at offsets: IndexSet) {
+        for index in offsets {
+            let book = books[index]
+            managedObjContext.delete(book)
+        }
+        
+        do {
+            try managedObjContext.save()
+        } catch {
+            print("Error deleting book: \(error)")
         }
     }
 }
