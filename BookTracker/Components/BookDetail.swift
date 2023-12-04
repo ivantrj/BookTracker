5fc9e464c7326fd6780bb507f4eb48009a2fb1db
ivan <ivantrajanovski@gmail.com>
2023-12-04 14:32:37 +0100

    simpify inbox and book detail design

--- BookTracker/Components/BookDetail.swift
+++ BookTracker/Components/BookDetail.swift
@@ -12,37 +12,26 @@ struct BookDetail: View {
     var author: String
 
     var body: some View {
-        HStack(spacing: 15) {
-            // Icon or Image for the book
-            Image(systemName: "book.closed")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 40, height: 40)
+        HStack {
+            Image(systemName: "book")
                 .foregroundColor(.blue)
+                .padding(.trailing, 10)
 
-            // Book Details
-            VStack(alignment: .leading, spacing: 5) {
+            VStack(alignment: .leading) {
                 Text(title)
                     .font(.headline)
-                    .foregroundColor(.primary) // Adapts to dark/light mode
-                    .lineLimit(1) // Ensures the title fits in a single line
-
+                    .foregroundColor(.primary)
                 Text(author)
                     .font(.subheadline)
-                    .foregroundColor(.secondary) // A slightly subdued color
-                    .lineLimit(1) // Fit the author's name in a single line
+                    .foregroundColor(.secondary)
             }
 
-            Spacer() // Pushes everything to the left
+            Spacer()
         }
-        .padding(.vertical, 8)
-        .padding(.horizontal, 10)
-        .background(Color(.systemBackground)) // Adapts to dark/light mode
-        .cornerRadius(10)
-        .shadow(color: Color(.systemGray4), radius: 5, x: 0, y: 2)
-        .padding(.horizontal, 10) // Additional padding for the list item
+        .padding()
     }
 }
+
 
 
 
--- BookTracker/Views/InboxView.swift
+++ BookTracker/Views/InboxView.swift
@@ -11,8 +11,7 @@ struct InboxView: View {
     @AppStorage("darkMode") var isDarkMode = false
     @Environment(\.managedObjectContext) var managedObjContext
     @FetchRequest(sortDescriptors: [SortDescriptor(\.date, order: .reverse)]) var books: FetchedResults<Book>
-    
-    @State private var showingAddBookView = false // State to control the presentation of the add book view
+    @State private var showingAddBookView = false
     
     var body: some View {
         NavigationStack {
@@ -22,31 +21,21 @@ struct InboxView: View {
                 }
                 .onDelete(perform: deleteBook)
             }
-            .padding()
             .navigationTitle("Inbox")
-            .preferredColorScheme(isDarkMode ? .dark : .light)
-        }
-        VStack {
-            Spacer()
-            HStack {
-                Spacer()
-                Button(action: {
-                    showingAddBookView = true
-                }) {
-                    Image(systemName: "plus")
-                        .resizable()
-                        .frame(width: 24, height: 24)
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .clipShape(Circle())
-                }
-                .padding()
-                .sheet(isPresented: $showingAddBookView) {
-                    AddBookView()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        showingAddBookView = true
+                    }) {
+                        Image(systemName: "plus")
+                    }
                 }
             }
+            .sheet(isPresented: $showingAddBookView) {
+                AddBookView()
+            }
         }
+        .preferredColorScheme(isDarkMode ? .dark : .light)
     }
     
     func deleteBook(at offsets: IndexSet) {
